b1b62a2440d98cfcca1b4d075975cef7811099fc
David <david.padilla@ibm.com>
2025-08-28 20:43:11 -0500

    casing

--- src/config/subgraph-config.js
+++ src/config/subgraph-config.js
@@ -2,10 +2,10 @@
 
 export const SUBGRAPH_CONFIG = {
   // TheGraph endpoint
-    URL: "https://api.studio.thegraph.com/query/119680/liberty-market-alpha/v0.0.3",
+    URL: "https://api.studio.thegraph.com/query/119680/liberty-market-alpha/v0.0.4",
 
     // The Treasury contract being tracked
-    PAIR_ADDRESS: "0xF46c7c417C1EC1fEA9240C6c1EcF293555F327f",
+    PAIR_ADDRESS: "0xf46c7c417c1ec1efa9240c6c1ecff293555f327f",
 
     // Default fetch options
     DEFAULT_INTERVAL: "60", // 1 minute candles
