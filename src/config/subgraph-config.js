9cdf0263f325bb56bad496dc50368bec6b37f1e5
David <david.padilla@ibm.com>
2025-08-30 12:52:54 -0500

    deploy file updates with address changes

--- src/config/subgraph-config.js
+++ src/config/subgraph-config.js
@@ -2,10 +2,10 @@
 
 export const SUBGRAPH_CONFIG = {
   // TheGraph endpoint
-    URL: "https://api.studio.thegraph.com/query/119680/liberty-market-alpha/v0.0.7",
+    URL: "https://api.studio.thegraph.com/query/119680/liberty-market-alpha/v0.1.2",
 
     // The Treasury contract being tracked
-    PAIR_ADDRESS: "0xe758e36476376ccddf574144ab3e9a560d550de3",
+    PAIR_ADDRESS: "0x90623b7e53af3243cb41c5b10954f1beaafe5e1d",
 
     // Default fetch options
     DEFAULT_INTERVAL: "60", // 1 minute candles
