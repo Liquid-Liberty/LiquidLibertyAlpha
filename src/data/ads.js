90e9745c327d3c5ce1497fe717d6888e4062fffd
David <david.padilla@ibm.com>
2025-09-06 13:08:46 -0500

    update ad sponsors

---
+++ src/data/ads.js
@@ -0,0 +1,12 @@
+export const localAds = {
+    cvre: {
+        imagePath: 'CVRE-ad.png',
+        link: 'https://www.cvrecrypto.com/',
+        altText: 'CVRE - The RWA seeking to democratize Real Estate!',
+    },
+    shillBanner: {
+        imagePath: 'Shill-bannerAd.png',
+        link: 'https://x.com/ImShillGates',
+        altText: 'Explore Shill on X!',
+    },
+};
