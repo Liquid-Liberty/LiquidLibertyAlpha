7bfecc40402f4fddf29df5f337937eccf854c9a0
David <david.padilla@ibm.com>
2025-09-10 13:47:24 -0500

    update UI to enable swapping of testnets

--- src/pages/ServiceCategoryPage.jsx
+++ src/pages/ServiceCategoryPage.jsx
@@ -17,7 +17,7 @@ const ServiceCategoryPage = ({ listings }) => {
                 {serviceListings.length > 0 ? (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                         {serviceListings.map(listing => (
-                            <ServiceListingCard key={listing.id} listing={listing} />
+                            <ServiceListingCard key={listing.uniqueId} listing={listing} />
                         ))}
                     </div>
                 ) : (
